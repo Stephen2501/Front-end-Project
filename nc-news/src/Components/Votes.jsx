922632b9595b3ec2c9205bca293e7811f9df2f26
Stephen Cocks <93137548+Stephen2501@users.noreply.github.com>
2022-03-09 11:44:50 +0000

    Fixed disable button

--- nc-news/src/Components/Votes.jsx
+++ nc-news/src/Components/Votes.jsx
@@ -4,7 +4,7 @@ import * as articleApi from '../utils/articleApi'
 export default function Votes({article, article_id}) {
 
     const [vote, setVote] = useState(0)
-    const [isDisabled, setIsDisabed] = useState(false)
+
 
     useEffect(() => {
         setVote(article.votes)
@@ -12,19 +12,21 @@ export default function Votes({article, article_id}) {
 
     const handleClick = (voteChange) => {
         setVote((currentVotes) => {
-            setIsDisabed(true)
+
             return (currentVotes + voteChange)
         })
         articleApi.voteOnArticle(article_id, {votes: voteChange});
     }
+    
+let voteChange = vote - article.votes
 
     return (
         <section>
             <div>
                 Votes: {vote}
             </div>
-            <button disabled={isDisabled} onClick={() => handleClick(1)} >Up-Vote</button>
-            <button disabled={isDisabled} onClick={() => handleClick(-1)} >Down-Vote</button>
+            <button disabled={voteChange === 1} onClick={() => handleClick(1)} >Up-Vote</button>
+            <button disabled={voteChange === -1} onClick={() => handleClick(-1)} >Down-Vote</button>
         </section>
     )
 }
