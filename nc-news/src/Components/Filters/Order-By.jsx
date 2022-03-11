d7c53d53041bbf58673ed829b8dc1497138ec9f2
Stephen Cocks <93137548+Stephen2501@users.noreply.github.com>
2022-03-11 17:41:33 +0000

    fixed order buttons

--- nc-news/src/Components/Filters/Order-By.jsx
+++ nc-news/src/Components/Filters/Order-By.jsx
@@ -3,8 +3,8 @@
 export default function OrderBy({setOrderBy}) {
     return (
         <div >
-        <button className="order-buttons" id='ASC' onClick={() => setOrderBy('ASC')} >High to low ↑↓</button>
-        <button className="order-buttons" id='DESC' onClick={() => setOrderBy('DESC')}>Low to high ↓↑</button>
+        <button className="order-buttons" id='ASC' onClick={() => setOrderBy('ASC')} >Low to high ↓↑</button>
+        <button className="order-buttons" id='DESC' onClick={() => setOrderBy('DESC')}>High to low ↑↓</button>
         </div>
     )
 }
