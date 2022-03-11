1f3b57282b6edef701fd5ea960877dc850be1733
Stephen Cocks <93137548+Stephen2501@users.noreply.github.com>
2022-03-11 11:10:17 +0000

    Errors added

---
+++ nc-news/src/Components/Error.jsx
@@ -0,0 +1,8 @@
+export default function Error() {
+    return (
+        <div>
+           URL does not exist. Please update URL or use any of the navigation tools above.
+        </div>
+    )
+
+}
