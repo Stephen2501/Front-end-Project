6f820b749afb135afc8516a710df24e1d2226504
Stephen Cocks <93137548+Stephen2501@users.noreply.github.com>
2022-04-04 14:51:48 +0100

    Added more info into message

--- nc-news/src/Components/Articles/Order-Message.jsx
+++ nc-news/src/Components/Articles/Order-Message.jsx
@@ -1,7 +1,12 @@
-export default function OrderMsg({orderBy}) {
-    if(orderBy === 'ASC') {
-        return <h3>Lowest to Highest</h3>
+export default function OrderMsg({orderBy, sortBy}) {
+
+    let sort = "";
+    if(sortBy === "created_at") sort = "date";
+    if(sortBy === "votes") sort = "votes";
+    if(sortBy === "comment_count") sort = "comment count";
+    if(orderBy === 'ASC') { 
+        return <h3>Lowest to Highest {sort}</h3>
     } else {
-        return <h3>Highest to Lowest</h3>
+        return <h3>Highest to Lowest {sort}</h3>
     }
 }
